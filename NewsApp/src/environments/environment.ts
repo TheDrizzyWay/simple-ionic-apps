85befbbf15e8aef72049e79a4cf23c9281df44ff
Christopher Iliyas <christopheriliyas@gmail.com>
2019-10-20 10:19:53 +0100

    create navigation setup

--- NewsApp/src/environments/environment.ts
+++ NewsApp/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '15f5f74f8b574da8ac9fbdb821dbb222'
 };
 
 /*
